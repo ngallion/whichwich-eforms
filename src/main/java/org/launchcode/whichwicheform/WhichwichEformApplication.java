bbddce4cf03e51a850e0dd0d71061591e984b507
Nick Gallion <nicholas.gallion@gmail.com>
2018-06-19 12:47:08 -0500

    Initial commit

---
+++ src/main/java/org/launchcode/whichwicheform/WhichwichEformApplication.java
@@ -0,0 +1,12 @@
+package org.launchcode.whichwicheform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WhichwichEformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WhichwichEformApplication.class, args);
+	}
+}
---
+++ src/test/java/org/launchcode/whichwicheform/WhichwichEformApplicationTests.java
@@ -0,0 +1,16 @@
+package org.launchcode.whichwicheform;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class WhichwichEformApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
